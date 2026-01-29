c30ff5a69dd6c8d219d03246063a0d5a90f6f998
AeonRagon <manos3291@gmail.com>
2026-01-29 14:37:24 +0200

    Basic Operators

--- assignments.js
+++ assignments.js
@@ -1,8 +1,31 @@
 // 01 VALUE AND VARIABLES
 const country = "Greece";
 const continent = "Europe";
-let population = "10.000.000";
-
+let population = 10000000;
 let isIsland = false;
 const language = "greek";
-console.log(country, continent, population, isIsland, language);
+
+console.log(
+  typeof country,
+  typeof continent,
+  typeof population,
+  typeof isIsland,
+  typeof language,
+);
+
+console.log(population / 2);
+population++;
+
+console.log(population > 600000);
+console.log(population > 33000000);
+
+const desricption =
+  country +
+  " is in " +
+  continent +
+  ", and its " +
+  population +
+  " people speak " +
+  language;
+
+console.log(desricption);
